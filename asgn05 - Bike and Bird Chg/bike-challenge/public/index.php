ad744d5290999d5afebdd92de5d20b299bac103d
JoeThomas42 <132302844+JoeThomas42@users.noreply.github.com>
2024-11-06 14:07:59 -0500

    bird-challenge completed

--- asgn05 - Bike and Bird Chg/bike-challenge/public/index.php
+++ asgn05 - Bike and Bird Chg/bike-challenge/public/index.php
@@ -2,10 +2,4 @@
 
 <?php include(SHARED_PATH . '/public_header.php'); ?>
 
-<div id="main">
-
-  <ul id="menu">
-    <li><a href="<?php echo url_for('/bicycles.php'); ?>">View Our Inventory</a></li>
-  </ul>
-    
-</div>
+<a href="<?php echo url_for('/bicycles.php'); ?>">View Our Inventory</a></li>
