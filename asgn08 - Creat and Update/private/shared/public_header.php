7b4b9806f02c31a96b3384b2cc23e52c8af99077
JoeThomas42 <132302844+JoeThomas42@users.noreply.github.com>
2024-11-20 19:04:29 -0500

    fixed css link

--- asgn08 - Creat and Update/private/shared/public_header.php
+++ asgn08 - Creat and Update/private/shared/public_header.php
@@ -3,10 +3,12 @@
 <html lang="en">
 
 <head>
+  <meta charset="utf-8">
   <title>WNC Birds <?php if (isset($page_title)) {
                       echo '- ' . h($page_title);
                     } ?></title>
-  <meta charset="utf-8">
+  <link rel="stylesheet" href="css/styles.css">
+
 </head>
 
 <body>
